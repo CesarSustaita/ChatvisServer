95b6893b37da84966799143630c0f662d4fac61c
LuisJavierRuiz <A286774@alumnos.uaslp.mx>
2023-11-24 16:14:41 -0600

    add implementation for login/register

--- serverBD.js
+++ serverBD.js
@@ -1,10 +1,19 @@
 // parte de variables para el documento
 const express = require("express");
+const cors = require("cors");
 const ConectarDB = require("./config/db");
+const corsOptions = {
+  origin: "http://localhost:5173",
+  methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+  credentials: true,
+  optionsSuccessStatus: 204,
+};
 
+// Habilita CORS con opciones específicas
 // parte de accesos de variables
 ConectarDB();
 const app = express();
+app.use(cors(corsOptions));
 app.use(express.json());
 app.use("/api/user", require("./routes/routes"));
 
